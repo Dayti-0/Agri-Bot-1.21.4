6dde8ac9fc95df040ac521906811bb90fc10546a
Nix <neogame34@gmail.com>
2026-01-05 19:17:54 +0100

    Add files via upload

---
+++ src/client/kotlin/fr/nix/agribot/AgriBotClient.kt
@@ -0,0 +1,9 @@
+package fr.nix.agribot
+
+import net.fabricmc.api.ClientModInitializer
+
+object AgriBotClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
